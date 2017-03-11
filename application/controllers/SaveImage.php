06d7371b81446df319479c5c08aa2067adff7604
Komkeao <kcsthai1995@gmail.com>
2017-03-11 18:58:40 +0700

    up load image has created

--- application/controllers/SaveImage.php
+++ application/controllers/SaveImage.php
@@ -5,28 +5,29 @@ class SaveImage extends CI_Controller {
 
 	public function index()
 	{
+		$this->uploadImage("ss","featured");
+	}
+	private function uploadImage($name,$inputName){
 		$config['upload_path'] = 'images';
 		$config['allowed_types'] = 'gif|jpg|jpeg|png';
-		$config['max_size'] = '10000';
+		$config['max_size'] = '1024';
 		$config['max_width'] = '1024';
 		$config['max_height'] = '1024';
 		$config['remove_spaces'] = TRUE;
+		$config['file_name'] = $name;
 		$this->load->library("upload",$config);
-		if ($this->upload->do_upload('featured')) {
-// Files Upload Success
-echo "OK Good";
-var_dump($this->upload->data('featured'));
- 
-} else {
-// Files Upload Not Success!!
-$errors = $this->upload->display_errors();
-echo $errors;
- 
-} // End else
+		if ($this->upload->do_upload($inputName)) {
+			// Files Upload Success
+			echo "OK Good";
+			$upload_data = $this->upload->data();  
+  $file_name =   $upload_data['file_name'];
+			echo $file_name;
+	 
+		} else {
+			// Files Upload Not Success!!
+			$errors = $this->upload->display_errors();
+			echo $errors;
+		}
 	}
-
 	
 }
-
-/* End of file SaveImage.php */
-/* Location: ./application/controllers/SaveImage.php */
